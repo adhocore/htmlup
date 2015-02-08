60ad8ba8cb70f41de6e72d26398101bee582b2b9
Jitendra Adhikari <jiten.adhikary@gmail.com>
2015-02-08 11:06:13 +0545

    fix bootstrap autoload, add some tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,11 @@
 <?php
 
 error_reporting(E_ALL);
-require __DIR__ . "/../vendor/autoload.php";
+
+// If we dont have globally autoloaded phpunit (maybe a phar) then 
+// autoload from composer installed local vendor path
+if (false === class_exists('PHPUnit_Framework_TestCase')) {
+    require __DIR__ . "/../vendor/autoload.php";
+}
+
+require __DIR__ . "/../src/HtmlUp.php";
--- tests/src/HtmlUpTest.php
+++ tests/src/HtmlUpTest.php
@@ -2,5 +2,78 @@
 
 class HtmlUpTest extends PHPUnit_Framework_TestCase
 {
-    //put your code here
+    /**
+     * @dataProvider dataSrc
+     *
+     */
+    public function testAll()
+    {
+        list($testName, $markdown, $expectedMarkup) = func_get_args();
+        $actualMarkup = (string) new HtmlUp($markdown);
+
+        $this->assertion($testName, $expectedMarkup, $actualMarkup);
+    }
+
+    /**
+     * Prototype of test data:
+     * <code>
+     *   array(
+     *      'test case name',
+     *      'markdown text',
+     *      'expected markup text'
+     *   )
+     * </code>
+     */
+    public function dataSrc()
+    {
+        return array(
+            array(
+                'Atx Header',
+'# HelloH1
+## HelloH2',
+                '<h1>HelloH1</h1><h2>HelloH2</h2>'
+            ),
+            array(
+                'Unordered List',
+'- Hello
+* HelloAgain',
+                '<ul><li>Hello</li><li>HelloAgain</li></ul>'
+            ),
+            array(
+                'H8 is Paragraph',
+                '######## NoHeader',
+                '<p>######## NoHeader</p>'
+            ),
+            array(
+                'Horizontal Rule',
+'
+
+***
+
+___
+',
+                '<hr /><hr />'
+            ),
+        );
+    }
+
+    protected function assertion($testName, $expected, $actual)
+    {
+        $this->assertEquals(
+            $this->normalizeMarkup($expected), 
+            $this->normalizeMarkup($actual), 
+            $testName
+        );
+    }
+
+    protected function normalizeMarkup($markup)
+    {
+        $markup = preg_replace(
+            array('/\>[^\S ]+/s', '/[^\S ]+\</s', ), 
+            array('>'           , '<'           , ),
+            $markup
+        );
+
+        return trim($markup);
+    }
 }
